4dd1a630fdfdfe3939f660ed6867b8dde0541f15
dperel <davidperel.pdf@gmail.com>
2015-09-05 22:12:15 -0400

    Name of current artist sent to DOM via jQuery

--- Spotify-Background-Processing/app/assets/javascripts/dynamicBarChart.js
+++ Spotify-Background-Processing/app/assets/javascripts/dynamicBarChart.js
@@ -1,14 +1,12 @@
-
 function dynamicBarChart(data){
- var w = 400,
+ var w = 900,
      h = 400,
-     b = 25;
+     b = 35;
  
- if (typeof data === 'undefined') { data = [{"id":434,"created_at":"2015-09-04T15:22:26.148Z","updated_at":"2015-09-04T15:22:26.148Z","name":"Chris Brown","followers":2570281},{"id":435,"created_at":"2015-09-04T15:22:27.800Z","updated_at":"2015-09-04T15:22:27.800Z","name":"Chris Brown","followers":2570281},{"id":436,"created_at":"2015-09-04T15:22:28.327Z","updated_at":"2015-09-04T15:22:28.327Z","name":"Wiz Khalifa","followers":2512515},{"id":437,"created_at":"2015-09-04T15:22:28.823Z","updated_at":"2015-09-04T15:22:28.823Z","name":"Eminem","followers":4332256},{"id":438,"created_at":"2015-09-04T15:22:29.440Z","updated_at":"2015-09-04T15:22:29.440Z","name":"2 Chainz","followers":610710},{"id":439,"created_at":"2015-09-04T15:22:30.258Z","updated_at":"2015-09-04T15:22:30.258Z","name":"Maroon 5","followers":5911079}];  };
-
+ if (typeof data === 'undefined') { data = {"id":434,"created_at":"2015-09-04T15:22:26.148Z","updated_at":"2015-09-04T15:22:26.148Z","name":"Chris Brown","followers":2570281}}
  
  var x = d3.scale.linear()
-     .domain([0,d3.max(data).id])
+     .domain([433,435])
      .range([0, w]);
 
   var y = d3.scale.linear()
@@ -26,31 +24,39 @@ var colorScale = d3.scale.linear()
  var chart = d3.select("body")
   .append("svg:svg")
   .attr("class", "chart")
-  .attr("width", w * data.length - 1)
+  .attr("width", w)
   .attr("height", h);
 
-chart.selectAll("rect")
-  .data(data)
-  .enter().append("svg:rect")
-  .attr("x", function(d) { return x(d.id)+250; })
-  .attr("y", function(d) { return h - y(d.followers)})
-  .attr("width", b)
-  .attr("height", function(d) { return heightScale(d.followers); });
+ chart.append("svg:line")
+    .attr("x1", 100)
+    .attr("x2", w )
+    .attr("y1", h - 0.5)
+    .attr("y2", h - 0.5)
+    .attr("stroke", "#000");
 
- setTimeout(function(){
-        updateChart();
+setTimeout(function(){
+  updateChart();
     }, 1100);
+
+// chart.append("svg:line")
+//    .attr("x1", 0)
+//    .attr("x2", 600)
+//    .attr("y1", h - .5)
+//    .attr("y2", h - .5)
+//    .attr("stroke", "#000");
+
 };
 
-
-  function redraw(data) {
-    console.log(data.length)
+function redraw(data) {
   
    var chart = d3.select("svg");
-   var w = 400,
+   
+   var w = 900,
       h = 400,
-      b = 25;
-   var x = d3.scale.linear()
+      b = 35;
+
+
+  var x = d3.scale.linear()
      .domain([0,d3.max(data).id])
      .range([0, w]);
  
@@ -58,45 +64,57 @@ chart.selectAll("rect")
      .domain([0, 3000000])
      .range([0, h]);
 
-var heightScale = d3.scale.linear()
+  var heightScale = d3.scale.linear()
     .domain([0,3000000])
     .range([0,h]);
 
-var colorScale = d3.scale.linear()
+  var colorScale = d3.scale.linear()
     .domain([0,3000000])
     .range(["blue","red"]);
 
 
-   var rect = chart.selectAll("rect")
+  var rect = chart.selectAll("rect")
        .data(data, function(d) { return d.created_at; });
- 
+
+
    // Enter…
    rect.enter().insert("svg:rect", "line")
-       .attr("x", function(d) { return x(d.id)+350})
+       .attr("x", function(d) { return x(d.id)})
        .attr("y", function(d) { return h - y(d.followers)})
        .attr("width", b)
        .attr("height", function(d) { return heightScale(d.followers); })
-       .attr("fill", function(d) { return colorScale(d.followers); })
+       .attr("fill", function(d) { return colorScale(d.followers)})
+       .append("svg:title")
+       .attr("text", function(d){return (d.name)})
        .transition()
        .duration(1000)
-       .attr("x", function(d) { return x(d.id)+250});
- 
+       .attr("x", function(d) { return x(d.id)-250});
+
+$('#label').html(data[0].name)
+
    // Update…
    rect.transition()
        .duration(1000)
-       .attr("x", function(d) { return x(d.id); });
+       .attr("x", function(d) { return x(d.id)-450; });
  
    // Exit…
    rect.exit()
       .transition()
       .duration(1000)
-      .attr("x", function(d) { return x(d.id)-250})
+      .attr("x", function(d) { return x(d.id)-800})
       .remove();
+
+    // rect.enter().insert("svg:text", "text")
+    // .data(data)
+    // .attr("x", 265 )
+    // .attr("y", 240 )
+    // .style("text-anchor", "middle")
+    // .attr("text", function(d){return (d.name)}); 
+
+
 
 
     setTimeout(function(){
         updateChart();
     }, 1100);
-
- 
  };
